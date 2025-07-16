b585f86292a833795411f8ef44b7968de428602d
Roman Rendon <Roman.Rendon123@gmail.com>
2025-07-16 03:32:22 -0500

    Added Page.java worked on activity_story.xml

---
+++ app/src/main/java/com/kirbits/thedragonscrystal/models/Page.java
@@ -0,0 +1,10 @@
+package com.kirbits.thedragonscrystal.models;
+
+public class Page {
+    public int id;
+    public String text;
+    public String choice_1_text;
+    public int choice_1_target;
+    public String choice_2_text;
+    public int choice_2_target;
+}
