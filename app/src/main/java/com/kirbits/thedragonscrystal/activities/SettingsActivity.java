5628049f0542a1466eee075a3aa1a293f6034183
matth <matthewperacio@gmail.com>
2025-07-23 20:45:27 -0300

    A

--- app/src/main/java/com/kirbits/thedragonscrystal/activities/SettingsActivity.java
+++ app/src/main/java/com/kirbits/thedragonscrystal/activities/SettingsActivity.java
@@ -3,6 +3,12 @@ package com.kirbits.thedragonscrystal.activities;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.kirbits.thedragonscrystal.R;
+import android.widget.Button;
+import android.app.AlertDialog;
+import com.kirbits.thedragonscrystal.utils.SaveManager;
+import android.widget.Toast;
+
+
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -19,17 +25,18 @@ public class SettingsActivity extends AppCompatActivity {
     private void showResetProgressDialog() {
         new AlertDialog.Builder(this)
                 .setTitle("Reset All Progress")
-                .setMessage("Are you sure you want to reset ALL game progress?")
+                .setMessage("Are you sure you want to reset ALL game progress? This will delete all save files and cannot be undone.")
                 .setPositiveButton("Reset All", (dialog, which) -> {
                     resetAllProgress();
                 })
                 .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
                 .show();
     }
+
     private void resetAllProgress() {
         for (int slot = 1; slot <= 3; slot++) {
             SaveManager.deleteSave(this, slot);
         }
         Toast.makeText(this, "All game progress has been reset.", Toast.LENGTH_LONG).show();
-        }
+    }
 }
